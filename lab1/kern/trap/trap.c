92ed9a1dad6a95176de8e74618b21fb42e00859a
CrisClaus <120363896+CrisClaus@users.noreply.github.com>
2024-09-27 19:16:52 +0800

    Update trap.c
    改回原来的+2

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -174,7 +174,7 @@ void exception_handler(struct trapframe *tf) {
             */
             cprintf("Exception type: breakpoint\n");
             cprintf("ebreak caught at %p \n",tf->epc);
-            tf->epc += 4;
+            tf->epc += 2;
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
